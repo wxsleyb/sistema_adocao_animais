1abae6b70489005c0972742cef336675f0296969
wxsleyb <92063559+wxsleyb@users.noreply.github.com>
2024-11-03 17:26:03 -0300

    feat: helpers para o usuário

---
+++ backend/helpers/get-token.js
@@ -0,0 +1,12 @@
+const getToken = (req) => {
+
+
+    const authHeader = req.headers.authorization
+
+    const token = authHeader.split(" ")[1]
+
+    return token
+}
+
+
+module.exports = getToken
