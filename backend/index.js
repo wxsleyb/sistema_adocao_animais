a0601c42e75991f6c4d3fa598d587b26ca9f8ba2
wxsleyb <92063559+wxsleyb@users.noreply.github.com>
2024-11-03 21:54:44 -0300

    feat: inclusão da rota Pet

--- backend/index.js
+++ backend/index.js
@@ -14,8 +14,10 @@ app.use(express.static('public'))
 // Routes
 
 const userRoutes = require('./routes/UserRoutes')
+const PetRoutes = require('./routes/PetRoutes')
 
 app.use('/users', userRoutes)
+app.use('/pets', PetRoutes)
 
 
 app.listen(5000)
