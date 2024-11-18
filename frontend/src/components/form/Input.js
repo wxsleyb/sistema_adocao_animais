e459caf3c433f95008e0b648af2be4acb7aa3033
wxsleyb <92063559+wxsleyb@users.noreply.github.com>
2024-11-18 00:54:01 -0300

    feat: componente e css input

---
+++ frontend/src/components/form/Input.js
@@ -0,0 +1,14 @@
+import styles from './Input.module.css'
+
+
+function Input({ type, text, name, placeholder, handleOnChange, value, multiple }) {
+
+    return (
+        <div className={styles.form_control}>
+            <label htmlFor={name}>{text}:</label>
+            <input type={type} text={text} name={name} id={name} placeholder={placeholder} onChange={handleOnChange} value={value} {...(multiple ? {multiple} : '')} />
+        </div>
+    )
+}
+
+export default Input;
