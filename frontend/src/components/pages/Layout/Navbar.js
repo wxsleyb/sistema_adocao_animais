0acfad82d30b710a6dbb953da1867b554e269603
wxsleyb <92063559+wxsleyb@users.noreply.github.com>
2024-11-18 00:52:14 -0300

    feat: Navbar html e css

---
+++ frontend/src/components/pages/Layout/Navbar.js
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom"
+
+import styles from './Navbar.module.css'
+
+import Logo from '../../../assets/img/logo.png'
+
+function Navbar() {
+
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.navbar_logo}>
+                <img src={Logo} alt="Get A Pet"/>
+                Get A Pet
+            </div>
+            <ul>
+                <li>
+                    <Link to="/">Adotar</Link>
+                </li>
+                <li>
+                    <Link to="/login">Entrar</Link>
+                </li>
+                <li>
+                    <Link to="/register">Cadastrar</Link>
+                </li>
+            </ul>
+        </nav>
+
+    )
+}
+
+export default Navbar
