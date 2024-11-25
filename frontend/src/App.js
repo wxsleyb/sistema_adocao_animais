328623ae7f550dc376e71c2ee36db00faf749db3
wxsleyb <92063559+wxsleyb@users.noreply.github.com>
2024-11-24 23:40:32 -0300

    feat: novos componentes, paginas e contexto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,48 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 //components
-import Navbar from './components/pages/Layout/Navbar';
-import Footer from './components/pages/Layout/Footer';
-import Container from './components/pages/Layout/Container';
+import Navbar from './components/pages/layout/Navbar';
+import Footer from './components/pages/layout/Footer';
+import Container from './components/pages/layout/Container';
 
 
 // pages
 import Home from './components/pages/Home';
 import Login from './components/pages/Auth/Login';
 import Register from './components/pages/Auth/Register';
+import Profile from './components/pages/User/Profile';
+import MyPets from './components/pages/Pet/MyPets';
+
+//context
+import { UserProvider } from './context/UserContext';
+import Message from './components/pages/layout/Message';
+import AddPet from './components/pages/Pet/AddPet';
 
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Container>
-        <Routes>
-          <Route path="/login" element={<Login />}>
-          </Route>
-          <Route path="/register" element={<Register />}>
-          </Route>
-          <Route path="/" element={<Home />}>
-          </Route>
-        </Routes>
-      </Container>
-      <Footer />
+      <UserProvider>
+        <Navbar />
+        <Message/>
+        <Container>
+          <Routes>
+            <Route path="/login" element={<Login />}>
+            </Route>
+            <Route path="/users/register" element={<Register />}>
+            </Route>
+            <Route path="/user/profile" element={<Profile />}>
+            </Route>
+            <Route path="/pet/mypets" element={<MyPets />}>
+            </Route>
+            <Route path="/pet/add" element={<AddPet />}>
+            </Route>
+            <Route path="/" element={<Home />}>
+            </Route>
+          </Routes>
+        </Container>
+        <Footer />
+      </UserProvider>
     </Router>
   )
 }
