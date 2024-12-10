f386f410c512f04c2d5da8307eacc0ab94bbb394
wxsleyb <92063559+wxsleyb@users.noreply.github.com>
2024-12-10 12:42:27 -0300

    feat: rota das novas telas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,11 +12,15 @@ import Login from './components/pages/Auth/Login';
 import Register from './components/pages/Auth/Register';
 import Profile from './components/pages/User/Profile';
 import MyPets from './components/pages/Pet/MyPets';
+import Message from './components/pages/layout/Message';
+import AddPet from './components/pages/Pet/AddPet';
+import EditPet from './components/pages/Pet/EditPet';
+import PetDetails from './components/pages/Pet/PetDetails';
 
 //context
 import { UserProvider } from './context/UserContext';
-import Message from './components/pages/layout/Message';
-import AddPet from './components/pages/Pet/AddPet';
+import MyAdoptions from './components/pages/Pet/MyAdoptions';
+
 
 
 function App() {
@@ -29,13 +33,19 @@ function App() {
           <Routes>
             <Route path="/login" element={<Login />}>
             </Route>
-            <Route path="/users/register" element={<Register />}>
+            <Route path="/register" element={<Register />}>
             </Route>
             <Route path="/user/profile" element={<Profile />}>
             </Route>
             <Route path="/pet/mypets" element={<MyPets />}>
             </Route>
             <Route path="/pet/add" element={<AddPet />}>
+            </Route>
+            <Route path="/pet/:id" element={<PetDetails />}>
+            </Route>
+            <Route path="/pet/myadoptions" element={<MyAdoptions />}>
+            </Route>
+            <Route path="/pet/edit/:id" element={<EditPet />}>
             </Route>
             <Route path="/" element={<Home />}>
             </Route>
